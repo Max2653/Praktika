3e9ba9dbd2d7f4927db48e64ca2e1f74e52c3898
eiviq <48763695+Max2653@users.noreply.github.com>
2025-06-05 13:08:16 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def get_length(self):
+        return self.__length
+
+    def get_width(self):
+        return self.__width
+
+    def area(self):
+        return self.__length * self.__width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def get_radius(self):
+        return self.__radius
+
+    def area(self):
+        return math.pi * self.__radius ** 2
+
+if __name__ == "__main__":
+    shapes = [Rectangle(5, 3), Circle(4)]
+    
+    for shape in shapes:
+        if isinstance(shape, Rectangle):
+            print(f"Rectangle (length={shape.get_length()}, width={shape.get_width()}) area: {shape.area()}")
+        elif isinstance(shape, Circle):
+            print(f"Circle (radius={shape.get_radius()}) area: {shape.area():.2f}")
